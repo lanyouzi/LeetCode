9608d1ba48d2850b89a4593b9d637009c7b759b9
mrk-lyz <mrk_lanyouzi@yeah.net>
2022-04-21 20:47:51 +0800

    update trie

--- templates/trie.cpp
+++ templates/trie.cpp
@@ -1,26 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+// Triee is commonly used to generate, query sets of strings as an alternative to Map.
+
 const int MAXN = 1e5 + 10;
-int trie[MAXN][26]; // trie[i][j] represent j-th child of i-th node
-bool flag[MAXN];      
-int idx;
-void insert(string s)  // insert string s into trie
+int trie[MAXN][26];         // trie[i][j] represent the index of j-th child of i-th node
+bool flag[MAXN];            // flag[i] represent if i-th string is a complete word
+int total = 0;              // total number of words including prefix
+void insert(string s)       // insert given string s into the trie
 {
     int p = 0;
     for (int i = 0; i < s.size(); i++) {
-        char ch = s[i] - 'a';
+        int ch = s[i] - 'a';
         if (!trie[p][ch]) {
-            trie[p][ch] = ++idx;
+            total++;
+            trie[p][ch] = total;
         }
         p = trie[p][ch];
     }
     flag[p] = true;
 }
-bool query(string s)  // query if current given s is in the trie
+bool query(string s)  // query if given string s is in the trie
 {
     int p = 0;
     for (int i = 0; i < s.size(); i++) {
-        char ch = s[i] - 'a';
+        int ch = s[i] - 'a';
         if (!trie[p][ch])
             return false;
         else {

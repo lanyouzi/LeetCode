72a6788c38b2c65c188f8d575305ec0f6cd49b84
mrk-lyz <mrk_lanyouzi@yeah.net>
2022-05-20 14:42:33 +0800

    update

---
+++ src/436.寻找右区间.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=436 lang=cpp
+ *
+ * [436] 寻找右区间
+ */
+
+// @lc code=start
+class Solution {
+public:
+    // Double pointers
+    vector<int> findRightInterval(vector<vector<int>>& intervals) {
+        vector<pair<int, int>> startIntervals;
+        vector<pair<int, int>> endIntervals;
+        int n = intervals.size();
+        for (int i = 0; i < n; i++) {
+            startIntervals.emplace_back(intervals[i][0], i);
+            endIntervals.emplace_back(intervals[i][1], i);
+        }
+        sort(startIntervals.begin(), startIntervals.end()); // order by starts
+        sort(endIntervals.begin(), endIntervals.end());     // order by ends
+        vector<int> ans(n, -1);
+        for (int i = 0, j = 0; i < n && j < n; i++) {
+            while (j < n && endIntervals[i].first > startIntervals[j].first) {
+                j++;
+            }
+            if (j < n) {
+                ans[endIntervals[i].second] = startIntervals[j].second;
+            }
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
